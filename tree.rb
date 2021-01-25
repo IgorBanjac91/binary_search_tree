5cb5a5d17ec5ae908fd77c72d12c6940d6663ebd
IgorBanjac91 <igor.banjac91@gmail.com>
2021-01-25 10:29:19 +0100

    finish implementation delete method

--- tree.rb
+++ tree.rb
@@ -6,25 +6,54 @@ class Tree
   def initialize(array = nil)
     @root = build_tree(array)
   end
-
+  
   def build_tree(arr)
     return nil if arr.nil?
     arr.sort!.uniq!
     return Node.new(value: arr[0]) if arr.size == 1
     if arr.size == 2
-     return Node.new( value: arr[0], left: nil, right: Node.new(value: arr[1]) )
+      return Node.new( value: arr[0], left: nil, right: Node.new(value: arr[1]) )
     end
     middle = (arr.size + 1)/2 - 1
     left  = arr[0..middle -1]
     right = arr[middle + 1..-1]
     Node.new( value: arr[middle], left: build_tree(left), right: build_tree(right) )
   end
-
+  
   def delete(elm, root = @root)
     if elm > root.value || elm == root.value
       if root.right.value == elm || root.value == elm
         if root.right.right && root.right.left
-         
+          if root.value == elm
+            node_to_remove = root
+            cont = root
+            subs = root
+          else
+            node_to_remove = root.right   #later do the case when the elm is = to the root
+            cont = root.right
+            subs = root.right
+          end
+
+          until cont.left.nil?
+            if cont.left.value > node_to_remove.value
+              cont = cont.left
+            else
+              cont = cont.right
+            end
+          end 
+          p node_to_remove.value
+          p cont.value
+          node_to_remove.value = cont.value
+          p subs.right.value 
+          until subs.right.value == elm
+            p subs.right.left
+            if subs.right.left.value == cont.value
+              subs.right.left = nil
+              return
+            end
+            subs = subs.right
+          end
+          return subs.right = cont.right
         elsif root.right.right.nil? && root.right.left
           return root.right = root.right.left
         elsif root.right.right && root.right.left.nil?
@@ -37,7 +66,8 @@ class Tree
     else
       if root.left.value == elm 
         if root.left.right && root.left.left
-
+          value = root.left
+          cont = root.left
         elsif root.left.right.nil? && root.left.left
           return root.left = root.left.left
         elsif root.left.right && root.left.left.nil?
@@ -63,11 +93,15 @@ end
 
 tree = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-tree.delete(2)
+tree.delete(5)
 
 puts "-----------------------\n\n\n"
 tree.pretty_print
 puts "\n\n-----------------------"
 
 
+tree_1 = Tree.new([1, 2, 3, 4, 6, 7, 8, 9])
 
+puts "-----------------------\n\n\n"
+tree_1.pretty_print
+puts "\n\n-----------------------"
